d576ccc65a2c8db521f4dc6c4152466505295c9e
Maksim <savelchevmaxim@gmail.com>
2021-04-02 20:27:53 +0400

    Change populateDB

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -1,3 +1,3 @@
 insert into CLIENTS (LAST_NAME, NAME, MIDDLE_NAME, EMAIL, TEL_NUMBER)
- values ('Савельчев', 'Максим',null,'savelchevmaxim@gmail.com',null),
-        ('Савельчева','Алена','Юрьевна',null,89272677702);
+ values ('Петрович', 'Петр','','petrovich@gmail.com',''),
+        ('Анисимова','Елена','Александровна','',8800735900);
